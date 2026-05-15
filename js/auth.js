51b15d052ee975f2a135207b2d65e029919ed1c4
INDRI GUNAWAN <mrindrigunawan45@gmail.com>
2026-05-15 22:48:25 +0700

    update latest style

--- js/auth.js
+++ js/auth.js
@@ -1,28 +1,112 @@
-import { supabaseClient } from './supabase.js'
-import { showToast, showLoading, hideLoading } from './utils.js'
+import { createClient }
 
-window.login = async () => {
+from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
-  const email = document.getElementById('email').value
-  const password = document.getElementById('password').value
+const SUPABASE_URL =
+  'https://ocakjidyndcojeapdsop.supabase.co'
 
-  showLoading()
+const SUPABASE_KEY =
+  'sb_publishable_sBVisHxIzGA1pEtlChR5pw_pcF-J3nm'
 
-  const { error } = await supabaseClient.auth.signInWithPassword({
-    email,
-    password
-  })
+const supabase =
+  createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+  )
 
-  hideLoading()
+// =========================
+// LOGIN PAGE
+// =========================
 
-  if (error) {
-    showToast(error.message, 'error')
-    return
+const loginForm =
+  document.getElementById('loginForm')
+
+if (loginForm) {
+
+  loginForm.addEventListener(
+    'submit',
+    async (e) => {
+
+      e.preventDefault()
+
+      const email =
+        document.getElementById('email').value
+
+      const password =
+        document.getElementById('password').value
+
+      const { error } =
+        await supabase.auth.signInWithPassword({
+
+          email,
+          password
+
+        })
+
+      if (error) {
+
+        alert(error.message)
+
+        return
+
+      }
+
+      window.location.href =
+        'dashboard.html'
+
+    }
+  )
+
+}
+
+// =========================
+// CEK LOGIN
+// =========================
+
+async function checkAuth() {
+
+  const {
+
+    data: { session }
+
+  } = await supabase.auth.getSession()
+
+  // kalau belum login
+
+  if (
+    !session &&
+    window.location.pathname
+      .includes('dashboard.html')
+  ) {
+
+    window.location.href =
+      'index.html'
+
   }
 
-  showToast('Login berhasil')
+}
 
-  setTimeout(() => {
-    window.location.href = 'dashboard.html'
-  }, 1000)
+checkAuth()
+
+// =========================
+// LOGOUT
+// =========================
+
+const logoutBtn =
+  document.getElementById('logoutBtn')
+
+if (logoutBtn) {
+
+  logoutBtn.addEventListener(
+    'click',
+    async () => {
+
+      await supabase.auth.signOut()
+
+      window.location.href =
+        'index.html'
+
+    }
+  )
+
 }
